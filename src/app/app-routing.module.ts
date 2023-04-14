b7d2a7a4a15c48630177e226e508f67be62ecd99
luca <lucajoglidze@gmail.com>
2023-04-15 02:58:03 +0400

    fix authentification render bug

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthComponent } from './auth/auth.component';
 
 const routes: Routes = [
   {
@@ -9,7 +10,7 @@ const routes: Routes = [
   },
   {
     path: 'auth',
-    loadChildren: () => import('./auth/auth.module').then((m) => m.AuthModule),
+    component:AuthComponent
   },
   {
     path: 'home',
