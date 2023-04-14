c45729f3329b4f5486b9a58d6de3dd1345aad433
luca <lucajoglidze@gmail.com>
2023-04-15 01:14:57 +0400

    clean a code

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,8 +4,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
-import { UserInfoComponent } from './user-info/user-info.component';
 import { AuthModule } from './auth/auth.module';
 import { HomeModule } from './home-page/home.module';
 
